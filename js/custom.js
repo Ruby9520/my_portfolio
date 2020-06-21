d4237ac3eb65eaa73ac20a2709e00417ba1e0405
rubykao <ruby850810@gmail.com>
2020-06-21 21:06:43 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,20 +8,21 @@ $(document).ready(function($){
    $(this).attr('cx', getRandom(1,100) + '%').attr('cy', '-' + getRandom(1,100)).attr('r', getRandom(1,6))
  })
  //if not stick to the top, than fixed, and skill bar animation
-$(window).on('scroll',function(){
+$(window).scroll(function(){
   if($(window).scrollTop()> 100){
     $('.navigation').addClass('offtop');
   }else if($(window).scrollTop()> 480){
     var $bar=$('.bar');
+    console.log($bar.find('#p90').css('width'));
+    if($bar.find('#p90').css('width')==0){
     $bar.find('#p90').animate({'width':'90%'});
     $bar.find('#p70').animate({'width':'70%'});
     $('#p60').animate({'width':'60%'});
     $('#p50').animate({'width':'50%'});
     //$('#p50').animate({'width':'50%'}, 'slow');
-  }else if{
-    $('.navigation').removeClass('offtop');
+    }
   }else{
-    $(window).off('scroll');
+  $('.navigation').removeClass('offtop');
   }
 })
 
