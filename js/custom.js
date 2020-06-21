8e1a613cd22554bc94cfff196fef614ad793152a
rubykao <ruby850810@gmail.com>
2020-06-21 15:43:10 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,14 +8,14 @@ $(document).ready(function($){
    $(this).attr('cx', getRandom(1,100) + '%').attr('cy', '-' + getRandom(1,100)).attr('r', getRandom(1,6))
  })
  //if not stick to the top, than fixed, and skill bar animation
-$(window).scroll(function(){
+$(window).one('scroll', function(){
   if($(window).scrollTop()> 480){
     var $bar=$('.bar');
-    $bar.find('#p90').animate({'width':'90%'}, 'slow');
-    $bar.find('#p70').animate({'width':'70%'}, 'slow');
-    $('#p60').animate({'width':'60%'}, 'slow');
-    $('#p50').animate({'width':'50%'}, 'slow');
-    return false;
+    $bar.find('#p90').animate({'width':'90%'});
+    $bar.find('#p70').animate({'width':'70%'});
+    $('#p60').animate({'width':'60%'});
+    $('#p50').animate({'width':'50%'});
+    //$('#p50').animate({'width':'50%'}, 'slow');
   }
   else if($(window).scrollTop()> 100){
     $('.navigation').addClass('offtop');
